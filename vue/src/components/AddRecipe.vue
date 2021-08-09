e2e20492d898d11418b4cfd26d9817e9cf9294c2
Katie <katie@katie.com>
2021-08-09 11:32:35 -0400

    Added DIY validation with steps to Add Recipe. You should be able to follow steps to do it on Create Meal and Meal Plan

--- vue/src/components/AddRecipe.vue
+++ vue/src/components/AddRecipe.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="add">
     <div>
+      <div v-if="validationFailMsg.length > 0"> <!--DIY Validation Step 4 Add a div that shows the error if there is one. You should style this yourself-->
+        Error: {{validationFailMsg}}
+      </div>
       <form>
         Recipe Name:
         <input
@@ -10,7 +13,11 @@
           required
         />
         Recipe instructions:
-        <textarea class="form-control" v-model="newRecipe.instructions" required></textarea>
+        <textarea
+          class="form-control"
+          v-model="newRecipe.instructions"
+          required
+        ></textarea>
         Recipe Description:
         <textarea class="form-control" v-model="newRecipe.description" />
         <multiselect
@@ -23,12 +30,11 @@
           :preserve-search="true"
           placeholder="Select Your Ingredient"
           track-by="ingredientId"
-          @remove="toggleUnSelectIngredient"
         ></multiselect>
         <div class="submit-btn">
-        <button type="submit" class="btn" v-on:click="createRecipe">
-          Add Recipe
-        </button>
+          <button type="submit" class="btn" v-on:click.prevent="createRecipe"> <!--DIY Validation Step 5 Don't refresh automatically so add the .prevent -->
+            Add Recipe
+          </button>
         </div>
       </form>
     </div>
@@ -51,31 +57,55 @@ export default {
         ingredients: [],
       },
       ingredients: [],
+      validationFailMsg: "", //DIY Validation Step 1 - add a message to the data. If the length is 0, all is well. Otherwise it contains deets for the validation fail
     };
   },
   methods: {
+    //DIY Validation Step 2 - add a method to do the validation. You can put everything here or just stuff that's not working. up to you
+    //set the validationFailMsg based on checking all the fields. Return true at the end if length is == 0 indicating all is well or false if tehre's issues
+    checkValidation() {
+      this.validationFailMsg = "";
+      if (this.newRecipe.recipeName.length === 0) {
+        this.validationFailMsg += "Recipe name is required\n";
+      }
+      if (this.newRecipe.instructions.length === 0) {
+        this.validationFailMsg += "Recipe instructions are required\n";
+      }
+      if (this.newRecipe.description.length === 0) {
+        this.validationFailMsg += "Recipe description is required\n";
+      }
+      if (this.newRecipe.ingredients.length === 0) {
+        this.validationFailMsg += "At least one ingredient is required";
+      }
+      return this.validationFailMsg.length ===0;
+    },
     createRecipe() {
-      recipeService
-        .addRecipe(this.newRecipe)
-        .then((response) => {
-          if (response.status === 200) {
-            alert("Recipe Succesfully Added");
-            // this.$router.push({ name: "home" });
-            this.newRecipe={}
-          }
-        })
-        .catch((error) => {
-          if (error.response) {
-            this.errorMsg =
-              "Error adding recipe. Response received was '" +
-              error.response.statusText +
-              "'.";
-          } else if (error.request) {
-            this.errorMsg = "Error adding recipe. Server could not be reached.";
-          } else {
-            this.errorMsg = "Error adding card. Request could not be created.";
-          }
-        });
+      if (this.checkValidation()) {//DIY Validation Step 3 - before you  call the service to call the back end do the validation an only make the call
+      //if the validation returns true
+        recipeService
+          .addRecipe(this.newRecipe) 
+          .then((response) => {
+            if (response.status === 200) {
+              alert("Recipe Succesfully Added");
+              // this.$router.push({ name: "home" });
+              this.newRecipe = {};
+            }
+          })
+          .catch((error) => {
+            if (error.response) {
+              this.errorMsg =
+                "Error adding recipe. Response received was '" +
+                error.response.statusText +
+                "'.";
+            } else if (error.request) {
+              this.errorMsg =
+                "Error adding recipe. Server could not be reached.";
+            } else {
+              this.errorMsg =
+                "Error adding card. Request could not be created.";
+            }
+          });
+      }
     },
     toggleUnSelectIngredient(ingredientId) {
       this.newRecipe.ingredients = this.newRecipe.ingredients.filter(
@@ -102,26 +132,25 @@ export default {
   margin-bottom: 10px;
 }
 .btn {
-  margin:0 0 10px;
-display:inline-block;
-padding:0.5em 3em;
-border:0.16em solid #F38630;
-margin:0 0.3em 0.3em 0;
-box-sizing: border-box;
-text-decoration:none;
-text-transform:uppercase;
-font-family:'Roboto',sans-serif;
-font-weight:400;
-background-color:white;
-text-align:center;
-transition: all 0.15s;
-color:black;
-
+  margin: 0 0 10px;
+  display: inline-block;
+  padding: 0.5em 3em;
+  border: 0.16em solid #f38630;
+  margin: 0 0.3em 0.3em 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  background-color: white;
+  text-align: center;
+  transition: all 0.15s;
+  color: black;
 }
-.btn:hover{
-  color:black;
-border-color:black;
-background-color: whitesmoke;
+.btn:hover {
+  color: black;
+  border-color: black;
+  background-color: whitesmoke;
 }
 form {
   display: flex;
@@ -135,11 +164,11 @@ form {
 }
 
 form {
-  border: #FA6900 2px solid;
+  border: #fa6900 2px solid;
   padding: 10px;
   border-radius: 10px;
 }
-.submit-btn{
+.submit-btn {
   margin: 10px 0 0 0;
 }
 </style>
